e4e9c76455960e8e7943c0b677b5423bdf3ef1c0
Issa <isasarhan.lz@gmail.com>
2022-08-29 17:38:08 +0300

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Meme from './components/Meme';
+
+function App() {
+  return (
+    <div>
+      <Header title = "Meme Generator"/>
+      <Meme/>
+    </div>
+  );
+}
+
+export default App;
